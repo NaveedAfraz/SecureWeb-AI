f60a27dc453ce296730731f2ab201ad23d163d79
NaveedAfraz <naveedafraz123@gmail.com>
2025-06-15 09:07:33 +0530

    chnages in backend auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,22 +2,14 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
+import User from '../models/auth.js';
 dotenv.config();
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
-const users = []; // Use MongoDB in future
+
 
 // Default user creation
-(async () => {
-  const passwordHash = await bcrypt.hash("password123", 10);
-  users.push({
-    id: "user1",
-    username: "testuser",
-    passwordHash,
-    roles: ["user"]
-  });
-})();
+ 
 
 // Middleware to protect routes
 export const authenticateToken = (req, res, next) => {
@@ -39,11 +31,12 @@ router.post("/register", async (req, res) => {
   if (!username || !password)
     return res.status(400).json({ message: "All fields are required" });
 
-  if (users.find(u => u.username === username))
-    return res.status(409).json({ message: "User already exists" });
+  const user = await User.findOne({ username });
+  if (user) return res.status(409).json({ message: "User already exists" });
 
   const passwordHash = await bcrypt.hash(password, 10);
-  users.push({ id: Date.now().toString(), username, passwordHash, roles: ["user"] });
+  const newUser = new User({ username, passwordHash });
+  await newUser.save();
 
     res.status(201).json({ message: "User registered successfully!" });
   } catch (err) {
@@ -56,7 +49,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try{
-  const user = users.find(u => u.username === username);
+  const user = await User.findOne({ username });
   if (!user || !(await bcrypt.compare(password, user.passwordHash)))
     return res.status(401).json({ message: "Invalid credentials" });
 
@@ -71,10 +64,11 @@ router.post("/login", async (req, res) => {
       sameSite: "Lax",
       maxAge: 3600000, // 1 hour
     });
-    res.json({ message: "Login successful!", token, username: user.username });
+
+    return res.status(200).json({ message: "Login successful!", token, username: user.username });
   } catch (err) {
     console.error("Login error:", err);
-    res.status(500).json({ message: "Server error during login." });
+    return res.status(500).json({ message: "Server error during login." });
   }
 });
 
@@ -82,10 +76,12 @@ router.get("/check", (req, res) => {
   const token = req.cookies.token;
   if (!token) return res.status(401).json({ message: "Not logged in" });
 
+   
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Invalid token" });
-    res.json({ message: "User authenticated", user });
+    return res.status(200).json({ message: "User authenticated", user });
   });
+
 });
 
 router.post("/logout", (req, res) => {
